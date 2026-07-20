8dfd6a399c304b5af4f46685015035d29cd4afa2
luciano <lucianofreiredigital29@gmail.com>
2026-07-20 16:14:10 -0300

    feat: codigo hero

---
+++ hero.js
@@ -0,0 +1,27 @@
+const slides = document.querySelectorAll(".slide");
+
+let currentSlide = 0;
+
+function showSlide(index) {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+
+  slides[index].classList.add("active");
+}
+
+function nextSlide() {
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+
+  showSlide(currentSlide);
+}
+
+showSlide(currentSlide);
+
+setInterval(() => {
+  nextSlide();
+}, 5000);
