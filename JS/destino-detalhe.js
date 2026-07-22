03706130b640f8ffa499da98f2e811bd895fb652
lucianofreire29 <lucianofreiredigital29@gmail.com>
2026-07-22 20:38:08 -0300

    feat: orçamento

--- JS/destino-detalhe.js
+++ JS/destino-detalhe.js
@@ -1,3 +1,5 @@
+
+
 import destinos from "./dados.js";
 
 
@@ -11,12 +13,19 @@ document.querySelector(".imagem-principal");
 const miniaturas =
 document.querySelector(".galeria-miniaturas");
 
+
 const destino = destinos.find(
     destino => destino.id === idDestino
 );
+
+
 if(destino){
-    document.querySelector(".destino-imagem").src =
-    destino.imagem;
+    const imagemHero = document.querySelector(".destino-imagem");
+
+    if(imagemHero){
+        imagemHero.src = destino.imagem;
+        imagemHero.alt = destino.nome;
+    }
     document.querySelector(".destino-imagem").alt =
     destino.nome;
     document.querySelector(".destino-titulo").textContent =
@@ -32,11 +41,17 @@ if(destino){
     document.querySelector(".destino-descricao").textContent =
     destino.descricao;
 
-    document.querySelector(".destino-preco").textContent =
+    const precoDestino = document.querySelector(".destino-preco");
+
+    if(precoDestino){
+
+    precoDestino.textContent =
     destino.preco.toLocaleString("pt-BR",{
         style:"currency",
         currency:"BRL"
     });
+
+}
 
     document.querySelector(".info-pais").textContent =
     destino.pais;
@@ -47,7 +62,8 @@ if(destino){
     document.querySelector(".info-categoria").textContent =
     destino.categoria;
 
-    imagemPrincipal.src = destino.imagem;
+    imagemPrincipal.src = destino.galeria[0];
+    
 let imagensHTML = "";
 destino.galeria.forEach((imagem)=>{
     imagensHTML += `
@@ -59,6 +75,8 @@ destino.galeria.forEach((imagem)=>{
 });
 
 miniaturas.innerHTML = imagensHTML;
+
+
 
 const imagens =
 document.querySelectorAll(".miniatura");
@@ -75,3 +93,89 @@ imagens.forEach((imagem)=>{
 });
 
 }
+
+
+
+
+// SIMULADOR DE ORÇAMENTO
+
+const pessoasInput =
+document.querySelector("#qtdPessoas");
+
+const diasInput =
+document.querySelector("#qtdDias");
+
+const hospedagemInput =
+document.querySelector("#tipoHospedagem");
+
+const passagemInput =
+document.querySelector("#passagem");
+
+const seguroInput =
+document.querySelector("#seguro");
+
+const valorFinal =
+document.querySelector("#valorFinal");
+
+function calcularOrcamento(){
+    if(!destino) return;
+
+    const pessoas =
+    Number(pessoasInput.value);
+
+    const dias =
+    Number(diasInput.value);
+
+    const hospedagem =
+    Number(hospedagemInput.value);
+
+    let valor = 
+    destino.preco *
+    pessoas *
+    hospedagem;
+
+    valor =
+    valor * (dias / 7);
+
+    if(passagemInput.checked){
+        valor += 800 * pessoas;
+    }
+
+
+    if(seguroInput.checked){
+        valor += 150 * pessoas;
+    }
+
+    valorFinal.textContent =
+    valor.toLocaleString("pt-BR",{
+        style:"currency",
+        currency:"BRL"
+    });
+}
+
+pessoasInput.addEventListener(
+"input",
+calcularOrcamento
+);
+
+diasInput.addEventListener(
+"input",
+calcularOrcamento
+);
+
+hospedagemInput.addEventListener(
+"change",
+calcularOrcamento
+);
+
+passagemInput.addEventListener(
+"change",
+calcularOrcamento
+);
+
+seguroInput.addEventListener(
+"change",
+calcularOrcamento
+);
+
+calcularOrcamento();
