b56a373c8de91f1d2ef84c6c23b54cd823f10fc6
karlabi-dev <karlabianca@gmail.com>
2026-07-31 16:05:05 -0300

    style: Reformatei o desagner de pacotes.

---
+++ admin/JS/promocoes.js
@@ -0,0 +1,13 @@
+const modal = document.querySelector("#modalPromocoes");
+
+const btnNovo = document.querySelector("#btnNovo");
+
+const fechar = document.querySelector("#fecharModal");
+
+btnNovo.addEventListener("click", () => {
+  modal.classList.add("ativo");
+});
+
+fechar.addEventListener("click", () => {
+  modal.classList.remove("ativo");
+});
